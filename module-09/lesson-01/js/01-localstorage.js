50c072d1d5ef22990dbc4cdbeec4c277a1d1e7ac
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2025-05-01 09:39:06 +0300

    prepared module 09 lesson 01

---
+++ module-09/lesson-01/js/01-localstorage.js
@@ -0,0 +1,28 @@
+/**
+ * LocalStorage
+ */
+
+const LS_KEY = "Array of names";
+const names = ["Alice", "Kate", "Emma"];
+
+/**
+ * Збереження
+ * Чому треба використовувати метод JSON.stringify
+ */
+
+
+/**
+ * Читання
+ * Чому треба використовувати метод JSON.parse
+ */
+
+
+/**
+ * Видалення
+ */
+
+
+/**
+ * LocalStorage не може зберігати функції
+ */
+
